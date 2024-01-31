d5a51973935fd93ff8e8c5e16de163f2867cc580
gokhankayalibay <84080781+gokhankayalibay@users.noreply.github.com>
2024-01-31 22:03:19 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_deneme2
+//
+//  Created by Gokhan Kayalibay on 14.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
